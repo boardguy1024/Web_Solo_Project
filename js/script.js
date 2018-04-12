38d92fc4970b6241ce9e58382fa19b8e10fc6b6c
park kyung suk <boardguy1024@gmail.com>
2018-04-12 09:13:02 +0900

    first init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// How To use JQuery
+
+/*=================================
+|   |   |   | Preloader
+=====================================*/
+$(window).on('load', function() {
+   $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut();
+});
