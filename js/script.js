b4063973c2cc89a04642df580353a0775a61aa0d
park kyung suk <boardguy1024@gmail.com>
2018-04-26 09:45:13 +0900

    waypointsライブラリーを使ってprogressBarをAnimation

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 /*=================================
 |   |   |   | Preloader
 =====================================*/
-$(window).on('load', function() {
-   $('#status').fadeOut();
+$(window).on('load', function () {
+    $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
 
@@ -12,12 +12,36 @@ $(window).on('load', function() {
 /*=================================
 |   |   |   | Team
 =====================================*/
-$(function() {
+$(function () {
     $("#team-members").owlCarousel({
-        items: 2, autoplay: true,smartSpeed: 700,loop: true,
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
     });
+});
+
+/*=================================
+|   |   |   | Progress Bars
+=====================================*/
+$(function () {
+
+    $("#progress-elementes").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+        });
+
+        this.destroy()
+    }, {
+        offset: "bottom-in-view"
+    });
+
 });
