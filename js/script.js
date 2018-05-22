a3aee336e9cb27029663651de34c10ba4ff5c90a
park kyung suk <boardguy1024@gmail.com>
2018-05-22 09:25:50 +0900

    test imonialsにowlCarouselを設定

--- js/script.js
+++ js/script.js
@@ -93,7 +93,22 @@ $(function() {
 });
 });
 
+/*=================================
+|   |   |   | Test Imonials
+=====================================*/
 
+$(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
 
 
 
